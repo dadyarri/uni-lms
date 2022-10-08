d1c8f7d2c1ee44560b52a8ea9cb75520d1c9ed14
ironalex <superzhelezov@gmail.com>
2022-10-08 13:53:06 +0300

    feat(db): add SolutionAttempt

---
+++ src/Models/SolutionAttempt.cs
@@ -0,0 +1,21 @@
+﻿namespace src.Models;
+
+/// <summary>
+/// Model, which representing attempt of solution of assignment
+/// </summary>
+public class SolutionAttempt : BaseModel {
+  /// <summary>
+  /// A piece of work that user is given to do
+  /// </summary>
+  public required object Assignment;
+
+  /// <summary>
+  /// The author of attempt
+  /// </summary>
+  public required User Author;
+
+  /// <summary>
+  /// Timestamp of submiting
+  /// </summary>
+  public required DateTime SubmittedAt;
+}
