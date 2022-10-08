52641e4e91fb89c4e8975f31e97d2feae8f4ba35
ironalex <superzhelezov@gmail.com>
2022-10-08 12:20:36 +0300

    feat(db): add BaseModel

---
+++ src/Models/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace src.Models;
+/// <summary>
+/// Parent model from which are inherited all other models
+/// </summary>
+public class BaseModel {
+  /// <summary>
+  /// Unique identifier 
+  /// </summary>
+  public Guid Id;  
+}
