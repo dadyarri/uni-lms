172fd90abd9deb27b05b44a4f7f9777378e463d0
ironalex <superzhelezov@gmail.com>
2022-10-08 14:23:44 +0300

    feat(db): add Deadline model

---
+++ src/Models/Deadline.cs
@@ -0,0 +1,26 @@
+﻿namespace src.Models;
+
+/// <summary>
+/// Model, which representing time by which assignment should be done
+/// </summary>
+public class Deadline {
+  /// <summary>
+  /// Piece of work that user is given to do
+  /// </summary>
+  public required Assignment Assignment;
+
+  /// <summary>
+  /// Group of student
+  /// </summary>
+  public required Group Group;
+
+  /// <summary>
+  /// Piece of group
+  /// </summary>
+  public int? Subgroup;
+
+  /// <summary>
+  /// Time by which assignmet schould be submitted
+  /// </summary>
+  public required DateTime SubmitBy;
+}
