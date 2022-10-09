892d868764372ed55670565464f8ef9a0475ac82
ironalex <superzhelezov@gmail.com>
2022-10-09 15:38:38 +0300

    feat(db):add RegisterCode model

---
+++ src/Models/RegisterCode.cs
@@ -0,0 +1,26 @@
+﻿namespace src.Models;
+
+/// <summary>
+/// Model, which representing code of registration
+/// </summary>
+public class RegisterCode : BaseModel {
+  /// <summary>
+  /// Code of registration
+  /// </summary>
+  public required string Code;
+
+  /// <summary>
+  /// Is code valid
+  /// </summary>
+  public required bool IsValid;
+
+  /// <summary>
+  /// User who use code 
+  /// </summary>
+  public User? UsedBy;
+
+  /// <summary>
+  /// Code registration time
+  /// </summary>
+  public required DateTime UsedAt;
+}
