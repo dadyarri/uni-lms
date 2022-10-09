b531d6122ad62f0c19f02195aac08f1929a998c5
ironalex <superzhelezov@gmail.com>
2022-10-09 15:42:48 +0300

    feat(db): add Course Model

---
+++ src/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿namespace src.Models;
+
+/// <summary>
+/// Model, which representing study course
+/// </summary>
+public class Course : BaseModel {
+  /// <summary>
+  /// name of course
+  /// </summary>
+  public required string Name;
+
+  /// <summary>
+  /// Abbreviation of course
+  /// </summary>
+  public string? Abbreviation;
+
+  /// <summary>
+  /// Semester of Course
+  /// </summary>
+  public int? Semester;
+
+  /// <summary>
+  /// Owner of Course
+  /// </summary>
+  public required User Owner;
+
+  /// <summary>
+  /// Groups that are assigned to the course
+  /// </summary>
+  public required List<Group>? AssignedGroups;
+}
