25c8fd0cd91e8a43b0aad096215180a9ae6d14f1
ironalex <superzhelezov@gmail.com>
2022-10-09 16:55:21 +0300

    feat(db): add LessonType model

---
+++ src/Models/LessonType.cs
@@ -0,0 +1,10 @@
+﻿namespace src.Models; 
+/// <summary>
+/// Model, which representing type of lesson
+/// </summary>
+public class LessonType {
+  /// <summary>
+  /// Name of type of lesson
+  /// </summary>
+  public required string Name;
+}
