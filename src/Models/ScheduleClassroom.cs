a9f16b264ee64870ca7de5d7a53be84f58ba6381
ironalex <superzhelezov@gmail.com>
2022-10-09 15:55:14 +0300

    feat(db): add ScheduleClassroom model

---
+++ src/Models/ScheduleClassroom.cs
@@ -0,0 +1,14 @@
+﻿namespace src.Models;
+/// <summary>
+/// Model, which representing classroom of schedule
+/// </summary>
+public class ScheduleClassroom : BaseModel {
+  /// <summary>
+  /// Classroom of lesson
+  /// </summary>
+  public required string Classroom;
+  /// <summary>
+  /// Building where classrom is located
+  /// </summary>
+  public int? Building;
+}
