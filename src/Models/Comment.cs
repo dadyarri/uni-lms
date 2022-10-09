4c769f0ce261be5669b016caa161cdebcd94afa1
ironalex <superzhelezov@gmail.com>
2022-10-09 15:48:21 +0300

    feat(db): add Comment Model

---
+++ src/Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿namespace src.Models;
+
+/// <summary>
+/// Model, which representing commet on solution attempt
+/// </summary>
+public class Comment : BaseModel {
+  /// <summary>
+  /// Text of comment
+  /// </summary>
+  public required string Text;
+
+  /// <summary>
+  /// Solution attempt to which a comment is written
+  /// </summary>
+  public required SolutionAttempt SolutionAttempt;
+
+  /// <summary>
+  /// Author of comment
+  /// </summary>
+  public required User Author;
+
+  /// <summary>
+  /// Parent comment of a comment
+  /// </summary>
+  public Comment? ParentComment;
+}
