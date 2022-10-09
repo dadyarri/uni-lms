4154f30f5d4185dc6953ec22658a9a888da66c73
ironalex <superzhelezov@gmail.com>
2022-10-09 15:52:11 +0300

    feat(db): add ScheduleTime model

---
+++ src/Models/ScheduleTime.cs
@@ -0,0 +1,16 @@
+﻿namespace src.Models;
+
+/// <summary>
+/// Model, which representing schedule time
+/// </summary>
+public class ScheduleTime : BaseModel {
+  /// <summary>
+  /// Time of the start of lesson
+  /// </summary>
+  public required TimeSpan TimeStart;
+
+  /// <summary>
+  /// Time of the end of lesson
+  /// </summary>
+  public required TimeSpan TimeEnd;
+}
