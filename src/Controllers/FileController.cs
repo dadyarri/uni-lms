cd58d6b146fb93b69b5061f835784f9af4098c77
ironalex <superzhelezov@gmail.com>
2022-10-13 13:13:36 +0300

    feat(api): add FileController

---
+++ src/Controllers/FileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using src.Models;
+
+
+namespace src.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FileController : ControllerBase {
+  [HttpPost]
+  [Produces("application/json")]
+  [ProducesResponseType(StatusCodes.Status201Created)]
+  public async Task<ActionResult<Attachment>> UploadFile() {
+    return Created(
+      "api/File",
+      new Attachment {
+        Checksum = "bb078fd350bdfbf74975735c3c7734d6",
+        FileUrl = "https://www.dadyarri.ru/images/index/webp/avatar.webp",
+        VisibleName = "dadyarri",
+      }
+    );
+  }
+}
