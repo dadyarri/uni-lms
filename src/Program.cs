4aecb5f0cc539aca9a2186d704374115a6e76d68
ironalex <superzhelezov@gmail.com>
2022-10-13 13:06:53 +0300

    feat(conf): configure swagger ui

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,37 @@
+using Microsoft.OpenApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+  options => {
+    options.SwaggerDoc(
+      "v1",
+      new OpenApiInfo {
+        Version = "v0.0.1",
+        Title = "File API",
+        Description =
+          "API for upload and work with attachments"
+      }
+    );
+  }
+);
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+if (app.Environment.IsDevelopment()) {
+  app.UseSwagger();
+  app.UseSwaggerUI(options =>
+  {
+    options.SwaggerEndpoint("swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+  });
 }
 
 app.UseHttpsRedirection();
