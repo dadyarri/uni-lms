428b8b7ef514802a52ff8a9d996ced7e6e0a71f8
ironalex <superzhelezov@gmail.com>
2022-10-17 20:51:15 +0300

    feat(api): add ApplicationJPIF

---
+++ src/Data/ApplicationJPIF.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
+
+
+namespace src.Data;
+
+/// <summary>
+/// PATCH request body formatter
+/// </summary>
+public class ApplicationJpif {
+  /// <summary>
+  /// Gets the PATCH request body formatter
+  /// </summary>
+  /// <returns></returns>
+  public static NewtonsoftJsonInputFormatter GetJsonPatchInputFormatter() {
+    var builder = new ServiceCollection()
+                  .AddLogging()
+                  .AddMvc()
+                  .AddNewtonsoftJson()
+                  .Services
+                  .BuildServiceProvider();
+
+    return builder
+           .GetRequiredService<IOptions<MvcOptions>>()
+           .Value
+           .InputFormatters
+           .OfType<NewtonsoftJsonInputFormatter>()
+           .First();
+  }
+}
