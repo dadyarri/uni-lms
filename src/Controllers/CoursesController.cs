c98fdf3f4367429490f6550039cc0911f7dd9e5a
ironalex <superzhelezov@gmail.com>
2022-10-17 20:53:13 +0300

    feat(api): add CoursesController

---
+++ src/Controllers/CoursesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using src.Models;
+
+
+namespace src.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CoursesController : ControllerBase {
+  [HttpGet]
+  [Produces("application/json")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  public async Task<ActionResult<Course>> GetById(Guid courseId) {
+    return Ok(
+      new Course {
+        Name = "Some course",
+        Owner = new User {
+          Email = "example@mail.com",
+          FirstName = "John",
+          LastName = "Doe",
+          Role = new Role {
+            Name = "Tutor",
+            Description = "",
+          },
+        },
+        AssignedGroups = new List<Group>(),
+      }
+    );
+  }
+}
