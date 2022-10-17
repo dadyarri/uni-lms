b33572603e36b0049ca6a34b6fc9ceee067c105e
ironalex <superzhelezov@gmail.com>
2022-10-17 21:06:08 +0300

    feat(api): add methods to FileController.cs GetFileById, DeleteId

--- src/Controllers/FileController.cs
+++ src/Controllers/FileController.cs
@@ -8,12 +8,38 @@ namespace src.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class FileController : ControllerBase {
+  [HttpGet]
+  [Produces("application/json")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  public async Task<ActionResult<Attachment>> GetFilebyId(Guid Id) {
+    return Ok(
+      new Attachment {
+        Checksum = "bb078fd350bdfbf74975735c3c7734d6",
+        FileUrl = "https://www.dadyarri.ru/images/index/webp/avatar.webp",
+        VisibleName = "dadyarri",
+      }
+    );
+  }
+
   [HttpPost]
   [Produces("application/json")]
   [ProducesResponseType(StatusCodes.Status201Created)]
   public async Task<ActionResult<Attachment>> UploadFile() {
     return Created(
       "api/File",
+      new Attachment {
+        Checksum = "bb078fd350bdfbf74975735c3c7734d6",
+        FileUrl = "https://www.dadyarri.ru/images/index/webp/avatar.webp",
+        VisibleName = "dadyarri",
+      }
+    );
+  }
+
+  [HttpDelete]
+  [Produces("application/json")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  public async Task<ActionResult<Attachment>> DeleteFile(Guid Id) {
+    return Ok(
       new Attachment {
         Checksum = "bb078fd350bdfbf74975735c3c7734d6",
         FileUrl = "https://www.dadyarri.ru/images/index/webp/avatar.webp",
