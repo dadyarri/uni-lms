254b6dc52d81e930220733e4c2dc208883318868
ironalex <superzhelezov@gmail.com>
2022-10-17 20:53:35 +0300

    feat(api): add response models

---
+++ src/Responses/Error.cs
@@ -0,0 +1,21 @@
+﻿namespace src.Responses;
+
+/// <summary>
+/// The body of the response returned when an error occurs
+/// </summary>
+public class Error {
+  /// <summary>
+  /// HTTP error code
+  /// </summary>
+  public required int Code { get; set; }
+
+  /// <summary>
+  /// Error message
+  /// </summary>
+  public required string Message { get; set; }
+
+  /// <summary>
+  /// Optional data describing the cause of the error
+  /// </summary>
+  public object? Data { get; set; }
+}
