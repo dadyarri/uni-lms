a1ee0529d98803397c2f83b699ed09481d6d6f8a
ironalex <superzhelezov@gmail.com>
2022-10-17 20:54:15 +0300

    feat(api): add request models

---
+++ src/RequestBodies/PagingParametrs.cs
@@ -0,0 +1,23 @@
+﻿namespace src.RequestBodies;
+
+/// <summary>
+/// Pagination parameters
+/// </summary>
+public class PagingParameters {
+  private const int MaxPageSize = 7;
+
+  private readonly int _pageSize;
+
+  /// <summary>
+  /// Number of page
+  /// </summary>
+  public int PageNumber { get; init; } = 1;
+
+  /// <summary>
+  /// Number of items per page
+  /// </summary>
+  public int PageSize {
+    get => _pageSize;
+    init => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+  }
+}
---
+++ src/RequestBodies/UserParameters.cs
@@ -0,0 +1,17 @@
+﻿using src.Models;
+
+
+namespace src.RequestBodies;
+
+/// <summary>
+/// Request body to Auth group methods
+/// </summary>
+public class UserParameters {
+  public required string FirstName { get; set; } = string.Empty;
+  public required string LastName { get; set; } = string.Empty;
+  public string? Patronymic { get; set; } = string.Empty;
+  public Group? Group { get; set; }
+  public int? Subgroup { get; set; }
+  public required Role Role { get; set; }
+  public required string Email { get; set; }
+}
