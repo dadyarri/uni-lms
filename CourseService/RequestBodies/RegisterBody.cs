e0d9a3e3c18f8bcd951c993dfbe33eee053dece3
Daniil <dadyarri@gmail.com>
2022-10-21 18:25:35 +0300

    feat(api): add model, representing body for register endpoint

---
+++ CourseService/RequestBodies/RegisterBody.cs
@@ -0,0 +1,16 @@
+﻿namespace src.RequestBodies; 
+
+/// <summary>
+/// Body for Register endpoint
+/// </summary>
+public class RegisterBody {
+  /// <summary>
+  /// Unique one-time register code
+  /// </summary>
+  public required string RegisterCode { get; set; }
+  
+  /// <summary>
+  /// Password of user
+  /// </summary>
+  public required string Password { get; set; }
+}
