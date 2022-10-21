5168f54a7411b486c2a4e748d6e669615c26a2c9
Daniil <dadyarri@gmail.com>
2022-10-21 09:29:34 +0300

    feat(docs): add some docstrings

--- CourseService/Controllers/CoursesController.cs
+++ CourseService/Controllers/CoursesController.cs
@@ -5,9 +5,17 @@ using src.Models;
 
 namespace src.Controllers;
 
+/// <summary>
+/// Controller for managing courses
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
 public class CoursesController : ControllerBase {
+  /// <summary>
+  /// Dummy method that returining course by its ID
+  /// </summary>
+  /// <param name="courseId">ID of course to find</param>
+  /// <returns>Model of course</returns>
   [HttpGet]
   [Produces("application/json")]
   [ProducesResponseType(StatusCodes.Status200OK)]
