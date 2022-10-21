cc459d0accc4700d7497caf1f1344ffcc82a6c04
Daniil <dadyarri@gmail.com>
2022-10-21 15:38:29 +0300

    feat(api): add login, whoami endpoints

--- CourseService/Controllers/AuthController.cs
+++ CourseService/Controllers/AuthController.cs
@@ -1,10 +1,14 @@
-﻿using System.Net;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Net.Mail;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 using Npgsql;
 
@@ -110,79 +114,127 @@ public class AuthController : ControllerBase {
     return Created("/api/Auth/Create", user);
   }
 
-  // /// <summary>
-  // /// Аутентификация пользователя и генерация JWT
-  // /// </summary>
-  // /// <param name="body">Логин и пароль пользователя</param>
-  // /// <response code="200">Токен сгенерирован</response>
-  // /// <response code="400">Невалидные данные (имя пользователя/пароль)</response>
-  // [AllowAnonymous]
-  // [HttpPost("Login")]
-  // [ProducesResponseType(StatusCodes.Status200OK)]
-  // [ProducesResponseType(StatusCodes.Status400BadRequest)]
-  // public async Task<ActionResult<string>> Login(UserParameters body) { }
-  //
-  // /// <summary>
-  // /// Получение информации о текущем пользователе
-  // /// </summary>
-  // /// <response code="200">Данные получены</response>
-  // /// <response code="401">Пользователь не авторизован</response>
-  // [Authorize]
-  // [HttpGet("Whoami")]
-  // [ProducesResponseType(StatusCodes.Status200OK)]
-  // [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-  // public ActionResult<UserResponse> Whoami() {
-  //   var user = GetCurrentUser();
-  //
-  //   if (user is not null) {
-  //     return Ok(user);
-  //   }
-  //
-  //   return Unauthorized(
-  //     new Error {
-  //       Code = (int) HttpStatusCode.Unauthorized,
-  //       Message = "User is not authorized"
-  //     }
-  //   );
-  // }
-  //
-  // private UserResponse? GetCurrentUser() {
-  //   if (HttpContext.User.Identity is not ClaimsIdentity identity) return null;
-  //   var userClaims = identity.Claims;
-  //   return new UserResponse {
-  //     Username = userClaims.First(c => c.Type == ClaimTypes.Name).Value
-  //   };
-  // }
-  //
-  // private string GenerateToken(User user) {
-  //   var claims = new List<Claim> {
-  //     new(ClaimTypes.Name, user.Username),
-  //   };
-  //
-  //   var rolesOfUser = _db.Roles.Where(
-  //     r => r.Users.Any(u => u.Id == user.Id)
-  //   );
-  //
-  //   foreach (var role in rolesOfUser) {
-  //     claims.Add(new Claim(ClaimTypes.Role, role.Name));
-  //   }
-  //
-  //   var key = new SymmetricSecurityKey(
-  //     System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("Security:Token").Value)
-  //   );
-  //
-  //   var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-  //
-  //   var token = new JwtSecurityToken(
-  //     claims: claims,
-  //     expires: DateTime.Now.AddMinutes(10),
-  //     signingCredentials: credentials
-  //   );
-  //
-  //   var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-  //
-  //   return jwt;
-  // }
+  /// <summary>
+  /// Authenticate user and generate JWT
+  /// </summary>
+  /// <param name="body">Login and password of user</param>
+  /// <response code="200">Token was generated</response>
+  /// <response code="400">Invalid credentials</response>
+  [AllowAnonymous]
+  [HttpPost("Login")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  public async Task<ActionResult<string>> Login(LoginBody body) {
+    var user = await _db.Users.FirstOrDefaultAsync(u => u.Email.Equals(body.Email));
+
+    if (user is null)
+    {
+      return BadRequest(new Error
+      {
+        Code = (int)HttpStatusCode.BadRequest,
+        Message = "User was not found",
+        Data = body.Email,
+      });
+    }
+
+    if (user.PasswordHash == null || user.PasswordSalt == null) {
+      throw new PasswordWasNotSetException();
+    }
+
+    if (!VerifyPasswordHash(body.Password, user.PasswordHash, user.PasswordSalt))
+    {
+      return BadRequest(new Error
+      {
+        Code = (int)HttpStatusCode.BadRequest,
+        Message = "Wrong password",
+      });
+    }
+
+    return Ok(GenerateToken(user));
+  }
+
+  /// <summary>
+  /// Fetching information about current user
+  /// </summary>
+  /// <response code="200">Data fetched</response>
+  /// <response code="401">Not authorized</response>
+  [Authorize]
+  [HttpGet("Whoami")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+  public ActionResult<UserResponse> Whoami() {
+    var user = GetCurrentUser();
+
+    if (user is not null) {
+      return Ok(user);
+    }
+
+    return Unauthorized(
+      new Error {
+        Code = (int)HttpStatusCode.Unauthorized,
+        Message = "User is not authorized",
+      }
+    );
+  }
+
+  private UserResponse? GetCurrentUser() {
+    if (HttpContext.User.Identity is not ClaimsIdentity identity) {
+      return null;
+    }
+
+    var userClaims = identity.Claims;
+    var email = userClaims.First(c => c.Type == ClaimTypes.Name).Value;
+    var user = _db.Users.First(u => u.Email.Equals(email));
+
+    return new UserResponse {
+      Email = user.Email,
+      FirstName = user.FirstName,
+      LastName = user.LastName,
+    };
+  }
+
+  private string GenerateToken(User user) {
+    var claims = new List<Claim> {
+      new(ClaimTypes.Name, user.Email),
+    };
+    var role = _db.Roles.First(r => r.Id == user.Role.Id);
+
+    claims.Add(new Claim(ClaimTypes.Role, role.Name));
+
+    string? securityToken;
+
+    try {
+      securityToken = _configuration.GetRequiredSection("Security").GetValue<string>("Token");
+    }
+    catch (InvalidOperationException) {
+      throw new MissingConfigurationValueException(
+        "Security.Token configuration value is required"
+      );
+    }
+
+    if (securityToken == null) {
+      throw new MissingConfigurationValueException(
+        "Security.Token configuration value is required"
+      );
+    }
+
+    var key = new SymmetricSecurityKey(
+      Encoding.UTF8.GetBytes(securityToken)
+    );
+
+    var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+    var token = new JwtSecurityToken(
+      claims: claims,
+      expires: DateTime.Now.AddMinutes(10),
+      signingCredentials: credentials
+    );
+
+    var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+    return jwt;
+  }
+
   private void CreatePasswordHash(
     string password, out byte[] passwordHash, out byte[] passwordSalt
   ) {
@@ -197,7 +249,7 @@ public class AuthController : ControllerBase {
     return computedHash.SequenceEqual(passwordHash);
   }
 
-  private string GenerateRandomString(int length) {
+  private static string GenerateRandomString(int length) {
     const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-!";
     Random random = new();
     return new string(
