a9d0cd971df6e62826d1d3fca05fedc639123711
Daniil <dadyarri@gmail.com>
2022-10-21 07:41:18 +0300

    feat(api): add DTO for attachment

---
+++ src/Dtos/AttachmentDto.cs
@@ -0,0 +1,24 @@
+﻿using src.Models;
+
+
+namespace src.Dtos; 
+
+/// <summary>
+/// Model, which representing aploaded attachment
+/// </summary>
+public class AttachmentDto : BaseModel {
+  /// <summary>
+  /// MD5 Checksum of file
+  /// </summary>
+  public required string Checksum { get; set; }
+
+  /// <summary>
+  /// Name of file which shows in UI
+  /// </summary>
+  public required string VisibleName { get; set; }
+
+  /// <summary>
+  /// Name of file
+  /// </summary>
+  public required string FileName { get; set; }
+}
