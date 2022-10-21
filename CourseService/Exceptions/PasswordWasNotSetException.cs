375db7cd90e0d8caa0ffb35e5b387aeccbe67b62
Daniil <dadyarri@gmail.com>
2022-10-21 15:26:40 +0300

    feat(exceptions): add one more exception

---
+++ CourseService/Exceptions/PasswordWasNotSetException.cs
@@ -0,0 +1,7 @@
+﻿namespace src.Exceptions; 
+
+public class PasswordWasNotSetException: Exception {
+  public PasswordWasNotSetException() {}
+
+  public PasswordWasNotSetException(string message): base(message) {}
+}
