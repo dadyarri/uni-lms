e13b0baa946904825ea83628959d75bb0dc06d27
Daniil <dadyarri@gmail.com>
2022-10-30 10:35:23 +0300

    feat(utils): add util to prepare multipart content from file

---
+++ CourseService/Utils/FileUploading.cs
@@ -0,0 +1,23 @@
+﻿namespace src.Utils;
+
+/// <summary>
+/// Set of utils to work with files
+/// </summary>
+public static class FileUploading {
+  /// <summary>
+  /// Method, which will prepare <see cref="MultipartFormDataContent"/> from <see cref="IFormFile"/>
+  /// </summary>
+  /// <returns>Prepared <see cref="IFormFile"/></returns>
+  public static async Task<MultipartFormDataContent> BuildFormDataContent(IFormFile file) {
+    var multipartFormData = new MultipartFormDataContent();
+    var ms = new MemoryStream();
+    await file.CopyToAsync(ms);
+    multipartFormData.Add(
+      new ByteArrayContent(ms.ToArray()),
+      file.FileName,
+      file.FileName
+    );
+
+    return multipartFormData;
+  }
+}
