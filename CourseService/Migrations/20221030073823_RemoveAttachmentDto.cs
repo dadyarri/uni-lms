df8fe83cbaf4b825fd1f54214da4e5c9cb607c20
Daniil <dadyarri@gmail.com>
2022-10-30 10:39:18 +0300

    fix(db): remove dependency on AttachmentDto table

---
+++ CourseService/Migrations/20221030073823_RemoveAttachmentDto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace src.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAttachmentDto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_AttachmentDto_AvatarId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "AttachmentDto");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_AvatarId",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "AvatarId",
+                table: "Users",
+                newName: "Avatar");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Avatar",
+                table: "Users",
+                newName: "AvatarId");
+
+            migrationBuilder.CreateTable(
+                name: "AttachmentDto",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Checksum = table.Column<string>(type: "text", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    VisibleName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttachmentDto", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AvatarId",
+                table: "Users",
+                column: "AvatarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_AttachmentDto_AvatarId",
+                table: "Users",
+                column: "AvatarId",
+                principalTable: "AttachmentDto",
+                principalColumn: "Id");
+        }
+    }
+}
