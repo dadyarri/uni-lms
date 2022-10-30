4b5744769b091ed33b89920d01de8d85d721a1b7
Daniil <dadyarri@gmail.com>
2022-10-30 11:20:52 +0300

    refactor: rename class

--- CourseService/Extensions/ConfigurationManagerExtensions.cs
+++ CourseService/Extensions/ConfigurationManagerExtensions.cs
@@ -3,15 +3,15 @@
 /// <summary>
 /// Set of extensions for configuration manager
 /// </summary>
-public static class ConfigurationExtensions {
+public static class ConfigurationManagerExtensions {
   /// <summary>
   /// Validator of configuration: Checks existing of required properties
   /// </summary>
   /// <param name="configuration">Manager of configuration (see <see cref="ConfigurationManager"/>)</param>
   /// <exception cref="InvalidOperationException">Throws, if any of required sections in configuration are missing</exception>
   public static void ValidateConfiguration(this ConfigurationManager configuration) {
-
-    var sections = new [] { "MailSettings:Address", "MailSettings:Token", "MailSettings:Host", "Security:Token" };
+    var sections = new[]
+      { "MailSettings:Address", "MailSettings:Token", "MailSettings:Host", "Security:Token" };
 
     foreach (var section in sections) {
       var _ = configuration.GetRequiredSection(section).Value;
